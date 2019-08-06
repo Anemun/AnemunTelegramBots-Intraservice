34deac05c3593f387231850b804476d577e4ef97
Anemun <anemun@gmail.com>
2019-08-06 14:45:05 +0300

    added watcher  details functional

--- jackIntrBot.py
+++ jackIntrBot.py
@@ -3,7 +3,7 @@ import argparse, sys
 import intraserviceProvider
 from telebot import types
 
-version = "0.1.3"
+version = "0.1.4"
 
 parser=argparse.ArgumentParser()
 parser.add_argument('--botToken', help='telegram bot token')
@@ -29,12 +29,19 @@ def start(message):
 def getFromWatcher(message):
     if intraserviceProvider.auth(message.from_user.username) is False:
         return
-    tickets = intraserviceProvider.getWatcher(login, password)
+    tickets = intraserviceProvider.getWatcher()
     keyboard = types.InlineKeyboardMarkup()
     for ticket in tickets:
-        button = types.InlineKeyboardButton(text=ticket.title, callback_data=ticket.id)
+        button = types.InlineKeyboardButton(text=ticket.title, callback_data="ticket_"+ str(ticket.id))
         keyboard.add(button)
     bot.send_message(chat_id=message.chat.id, text="Диспетчер: " + str(len(tickets)) + " заявок",reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data.split('_')[0] == "ticket":
+        ticketId = call.data.split('_')[1]
+    ticket = intraserviceProvider.getTicketById(ticketId)
+    bot.send_message(chat_id=call.message.chat.id, text="ID: {0}\nCreator: {1}\nText: {2}".format(ticket.id, ticket.creatorName, ticket.title))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
