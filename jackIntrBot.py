f1fc0428a755f6128baff21c47f2992d95c6e3a1
Anemun <anemun@gmail.com>
2019-08-06 14:11:20 +0300

    fixed docker

--- jackIntrBot.py
+++ jackIntrBot.py
@@ -3,7 +3,7 @@ import argparse, sys
 import intraserviceProvider
 from telebot import types
 
-version = "0.1.1"
+version = "0.1.2"
 
 parser=argparse.ArgumentParser()
 parser.add_argument('--botToken', help='telegram bot token')
