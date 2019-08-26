5d05c830b1bb39ff34cb29cd3006810a8fe2d6a4
Anemun <anemun@gmail.com>
2019-08-26 12:56:20 +0300

    added automatic watcher

--- jackIntrBot.py
+++ jackIntrBot.py
@@ -1,6 +1,6 @@
 import telebot
-import argparse, sys
-import intraserviceProvider
+import argparse, sys, os
+import intraserviceProvider, watcher
 from telebot import types
 
 version = "0.1.4"
@@ -19,13 +19,25 @@ password = args.sitePass
 intraserviceProvider.login = login
 intraserviceProvider.password = password
 
+try:
+    os.makedirs('./data/chats')
+except OSError:
+    pass
+if os.path.isfile('data/users.list') is False: 
+    open('data/users.list','w+')
+
+
+
 @bot.message_handler(commands=["start"])
 def start(message):
-    id = intraserviceProvider.getIntrIdByUsername(message.from_user.username)
+    if intraserviceProvider.auth(message.from_user.username) is False:
+        return
     if id is not None:
-        bot.send_message(message.chat.id, "Вы можете пользоваться ботом. Попробуйте следующие команды:\n/watcher")
+        bot.send_message(message.chat.id, "Вы можете пользоваться ботом. Попробуйте следующие команды:\n/fresh\n/watch\n/stop")
+    else:
+        bot.send_message(message.chat.id, "Использование данного бота не авторизовано. Обратитесь к администратору.")
 
-@bot.message_handler(commands=["watcher"])
+@bot.message_handler(commands=["fresh"])
 def getFromWatcher(message):
     if intraserviceProvider.auth(message.from_user.username) is False:
         return
@@ -36,12 +48,37 @@ def getFromWatcher(message):
         keyboard.add(button)
     bot.send_message(chat_id=message.chat.id, text="Диспетчер: " + str(len(tickets)) + " заявок",reply_markup=keyboard)
 
+@bot.message_handler(commands=["watch"])
+def subscribeForWatcher(message):
+    if intraserviceProvider.auth(message.from_user.username) is False:
+        return
+    result = watcher.addChatToWatcher(message.chat.id)        
+    if result == 'added':
+        bot.send_message(chat_id=message.chat.id, text="Данный чат теперь подписан на обновления диспетчера")
+        return
+    if result == 'already':
+        bot.send_message(chat_id=message.chat.id, text="Данный чат уже имеет подписку на обновления диспетчера")
+        return
+
+@bot.message_handler(commands=["stop"])
+def stop(message):
+    pass
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     if call.data.split('_')[0] == "ticket":
         ticketId = call.data.split('_')[1]
     ticket = intraserviceProvider.getTicketById(ticketId)
-    bot.send_message(chat_id=call.message.chat.id, text="ID: {0}\nCreator: {1}\nText: {2}".format(ticket.id, ticket.creatorName, ticket.title))
+    bot.send_message(chat_id=call.message.chat.id, text="№: {0}\nСоздатель: {1}\nНазвание: {2}".format(ticket.id, ticket.creatorName, ticket.title))
+
+def sendWatcherUpdates(chatId, tickets):
+    keyboard = types.InlineKeyboardMarkup()
+    for ticket in tickets:
+        button = types.InlineKeyboardButton(text=ticket.title, callback_data="ticket_"+ str(ticket.id))
+        keyboard.add(button)
+    bot.send_message(chat_id=chatId, text="В диспетчере появилось новых заявок: {0}".format(len(tickets)),reply_markup=keyboard)
+
+watcher.initWatcher(bot, sendWatcherUpdates)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
---
+++ watcher.py
@@ -0,0 +1,80 @@
+import intraserviceProvider, os
+import Ticket
+from threading import Timer
+
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+class chatToWatch:
+    def __init__(self, newChatID):
+        self.chatID = newChatID
+        self.acknowledged = []
+        self.getTicketsFromFile()
+        
+    def addTicketToAcnowledged(self, ticketId):
+        with open('data/chats/{0}.chat'.format(self.chatID), 'a+') as file:
+            file.write('{0}\n'.format(ticketId))
+        self.getTicketsFromFile()
+
+    def getTicketsFromFile(self):
+        if os.path.isfile('data/chats/{0}.chat'.format(self.chatID)) is True:
+            content = open('data/chats/{0}.chat'.format(self.chatID),'r').read()
+            if len(content) > 0:
+                self.acknowledged = content.split('\n')
+
+
+
+chatsForWatcher = {}
+telebot = None
+updateFunction = None
+
+def initWatcher(bot, fun):
+    global chatsForWatcher, telebot, updateFunction
+    chatsForWatcher = {}
+    telebot = bot
+    updateFunction = fun
+    if os.path.isfile('data/chats.list') is True:
+        content = open('data/chats.list','r+').read()
+        if len(content) > 0:
+            for chatId in content.split('\n'):
+                if chatId is "": continue           # skip an empty line at the end of the file
+                chatsForWatcher[chatId] = chatToWatch(chatId)
+    timerWatcher = RepeatTimer(10, sendUpdateFromWatcher)    
+    timerWatcher.start()
+
+def addChatToWatcher(chatId):
+    with open('data/chats.list', 'a+') as file:
+        file.seek(0)
+        content = file.read()     
+        ids = content.split('\n')
+        if str(chatId) not in ids:
+            file.write('{0}\n'.format(chatId))
+            chatsForWatcher[chatId] = chatToWatch(chatId)
+            return 'added'
+        else:
+            return 'already'
+
+def sendUpdateFromWatcher():
+    watcherTickets = intraserviceProvider.getWatcher()
+    if len(watcherTickets) > 0:
+        ticketIDs = [ticket.id for ticket in watcherTickets]
+        for chat in chatsForWatcher:
+            newTickets = filterNewTickets(chat, watcherTickets)
+            if len(newTickets) > 0:
+                updateFunction(chat, newTickets)
+                pass
+    # check for tickets in watcher list
+    # for each chatID check is any of tickets is new
+    # if it it, form a list and send it.
+    pass
+
+# filter already acknoledged tickets from received list
+def filterNewTickets(chatID, tickets):
+    newTickets = []
+    for ticket in tickets:
+        if str(ticket.id) not in chatsForWatcher[chatID].acknowledged:
+            chatsForWatcher[chatID].addTicketToAcnowledged(ticket.id)
+            newTickets.append(ticket)
+    return newTickets
